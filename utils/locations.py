e0a6d52a07d2b7ee42a05cb07f8eaae793d1cf44
ruslanyootuber-ux <ruslanyootuber@gmail.com>
2026-06-26 01:45:37 +0500

    Create utils/locations.py

---
+++ utils/locations.py
@@ -0,0 +1,16 @@
+# O'zbekiston viloyatlari va tumanlari ro'yxati
+UZB_REGIONS = {
+    "Toshkent": ["Yunusobod", "Chilonzor", "Mirzo Ulug'bek", "Sergeli", "Yashnobod", "Olmazor", "Uchtepa", "Yakkasaroy", "Shayxontohur", "Mirobod", "Bektemir", "Yangihayot"],
+    "Samarqand": ["Samarqand shahri", "Urgut", "Pastdarg'om", "Kattaqo'rg'on", "Bulung'ur", "Jomboy", "Ishtixon", "Oqdaryo", "Poyariq", "Nurobod", "Qo'shrabot", "Toyloq", "Paxtachi", "Narpay"],
+    "Andijon": ["Andijon shahri", "Asaka", "Shahrixon", "Xo'jaobod", "Buloqboshi", "Qo'rg'ontepa", "Jalaquduq", "Marhamat", "Paxtaobod", "Izboskan", "Ulug'nor", "Bo'z", "Oltinko'l"],
+    "Farg'ona": ["Farg'ona shahri", "Marg'ilon", "Qo'qon", "Beshariq", "Oltiariq", "Qo'shtepa", "Bag'dod", "Buvayda", "Dang'ara", "Uchko'prik", "Rishton", "So'x", "Toshloq", "Yozyovon", "Quva", "Farg'ona tumani"],
+    "Namangan": ["Namangan shahri", "Chust", "Kosonsoy", "Uychi", "To'raqo'rg'on", "Mingbuloq", "Norin", "Pop", "Uchqo'rg'on", "Yangiqo'rg'on", "Chortoq", "Davlatobod"],
+    "Buxoro": ["Buxoro shahri", "G'ijduvon", "Vobkent", "Shofirkon", "Peshku", "Romitan", "Jondor", "Qorako'l", "Olot", "Qorovulbozor", "Kogon"],
+    "Xorazm": ["Urganch", "Xiva", "Xonqa", "Gurlan", "Shovot", "Yangiariq", "Yangibozor", "Qo'shko'pir", "Hazorasp", "Bog'ot"],
+    "Surxondaryo": ["Termiz", "Denov", "Sariosiyo", "Sho'rchi", "Qumqo'rg'on", "Jarqo'rg'on", "Angor", "Muzrabot", "Sherobod", "Boysun", "Qiziriq", "Bandixon", "Oltinsoy"],
+    "Qashqadaryo": ["Qarshi", "Shahrisabz", "Kitob", "Yakkabog'", "Qamashi", "G'uzor", "Kasbi", "Nishon", "Mirishkor", "Koson", "Muborak", "Chiroqchi", "Dehqonobod", "Ko'kdala"],
+    "Jizzax": ["Jizzax shahri", "Do'stlik", "Zarbdor", "Zafarobod", "Zomin", "Mirzacho'l", "Paxtakor", "Forish", "Sharof Rashidov", "G'allaorol", "Baxmal", "Yangiobod"],
+    "Navoiy": ["Navoiy shahri", "Zarafshon", "Karmana", "Qiziltepa", "Xatirchi", "Navbahor", "Nurota", "Tomdi", "Uchquduq", "Konimex"],
+    "Sirdaryo": ["Guliston", "Yangiyer", "Shirin", "Sirdaryo tumani", "Boyovut", "Oqoltin", "Sayxunobod", "Sardoba", "Xovos", "Mirzaobod"],
+    "Qoraqalpog'iston": ["Nukus", "Xo'jayli", "Taxiatosh", "Chimboy", "Qo'ng'irot", "Mo'ynoq", "Amudaryo", "Beruniy", "To'rtko'l", "Qorao'zak", "Kegeyli", "Shumanay", "Qanliko'l", "Taxtako'pir", "Bo'zatov"]
+}
