e8029e4ee64ff9c89da1c5d15226ca18ab742ab7
ruslanyootuber-ux <ruslanyootuber@gmail.com>
2026-06-26 02:42:11 +0500

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -2,14 +2,17 @@ from aiogram import Router
 from aiogram.types import Message
 from aiogram.filters import CommandStart
 from keyboards.inline_kb import get_regions_keyboard
+from loader import db  # Bazani import qilamiz
 
 router = Router()
 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
-    print("LOG: /start komandasi muvaffaqiyatli ushlandi!") # Terminalda ko'rish uchun
+    # Foydalanuvchini bazaga qo'shish
+    db.add_user(message.from_user.id, message.from_user.full_name)
+    
     text = (
         "🕌 <b>Assalomu alaykum!</b>\n\n"
-        "O'zingizga kerakli viloyatni tanlang:"
+        "Xush kelibsiz! O'zingizga kerakli viloyatni tanlang:"
     )
     await message.answer(text, reply_markup=get_regions_keyboard())
