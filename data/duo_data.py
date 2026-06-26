11e9c7a6635eec313dea64c999c58c85b9629b2e
ruslanyootuber-ux <ruslanyootuber@gmail.com>
2026-06-26 10:07:52 +0500

    Create duo_data.py

---
+++ data/duo_data.py
@@ -0,0 +1,19 @@
+DUOLAR = {
+    "tong": [
+        {
+            "title": "Tonggi duo",
+            "ar": "اللَّهُمَّ بِكَ أَصْبَحْنَا، وَبِكَ أَمْسَيْنَا، وَبِكَ نَحْيَا، وَبِكَ نَمُوتُ، وَإِلَيْكَ النُّشُورُ",
+            "lat": "Allohumma bika asbahna va bika amsayna va bika nahya va bika namutu va ilaykan-nushur.",
+            "uz": "Allohim, Sening noming bilan tong otdi, Sening noming bilan kech kirdi. Sening noming bilan tirilamiz, Sening noming bilan o‘lamiz va (qiyomatda) Sening huzuringga qaytamiz."
+        }
+    ],
+    "kech": [
+        {
+            "title": "Kechki duo",
+            "ar": "اللَّهُمَّ بِكَ أَمْسَيْنَا، وَبِكَ أَصْبَحْنَا، وَبِكَ نَحْيَا، وَبِكَ نَمُوتُ، وَإِلَيْكَ الْمَصِيرُ",
+            "lat": "Allohumma bika amsayna va bika asbahna va bika nahya va bika namutu va ilaykal-masiir.",
+            "uz": "Allohim, Sening noming bilan kech kirdi, Sening noming bilan tong otdi. Sening noming bilan tirilamiz, Sening noming bilan o‘lamiz va Sening huzuringga qaytamiz."
+        }
+    ],
+    # Boshqa duolarni ham shu tartibda qo'shib ketaverasiz
+}
