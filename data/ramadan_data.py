5fa7a0af418e3a0f5f059b4863c29b85b4bd19e7
ruslanyootuber-ux <ruslanyootuber@gmail.com>
2026-06-26 05:42:08 +0500

    Create data/ramadan_data.py

---
+++ data/ramadan_data.py
@@ -0,0 +1,17 @@
+# data/ramadan_data.py
+
+SAHARLIK_DUO = """
+<b>Saharlik duosi:</b>
+<i>Navaytu an asuma sovma shahri ramazona minal fajri ilal mag'ribi, xolisan lillahi ta'ala. Allohu akbar.</i>
+
+<b>Ma'nosi:</b>
+Ramazon oyining ro'zasini tongdan to quyosh botguncha tutishni niyat qildim. Xolis Alloh uchun. Alloh buyukdir.
+"""
+
+IFTORLIK_DUO = """
+<b>Iftorlik duosi:</b>
+<i>Allohumma laka sumtu va bika aamantu va 'alayka tavakkaltu va 'alaa rizqika aftartu, fag'firlii yaa g'offaaru maa qoddamtu va maa axxortu.</i>
+
+<b>Ma'nosi:</b>
+Ey Alloh, Sen uchun ro'za tutdim, Senga iymon keltirdim, Senga tavakkal qildim va bergan rizqing bilan iftor qildim. Ey gunohlarni kechiruvchi Zot, mening avvalgi va keyingi gunohlarimni kechirgil.
+"""
