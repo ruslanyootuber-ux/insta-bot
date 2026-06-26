5c147ca7978d6258701bc856b3aa2a2e1f23f137
ruslanyootuber-ux <ruslanyootuber@gmail.com>
2026-06-26 05:50:55 +0500

    Update inline_kb.py

--- keyboards/inline_kb.py
+++ keyboards/inline_kb.py
@@ -21,7 +21,10 @@ def get_regions_keyboard() -> InlineKeyboardMarkup:
     builder.row(
         InlineKeyboardButton(text="🤲 Tonggi va Kechki zikrlar", callback_data="menu_zikr_main")
     )
-    
+    #  Ramazon taqvimi tugmasi
+    builder.row(
+        InlineKeyboardButton(text="🌙 Ramazon taqvimi", callback_data="ramadan_today")
+    )
     # Eslatma va Sozlamalar
     builder.row(
         InlineKeyboardButton(text="🔔 Eslatma", callback_data="menu_reminder"),
