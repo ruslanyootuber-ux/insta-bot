836002e6c4f842c0f4311f78357620202d95e7a7
ruslanyootuber-ux <ruslanyootuber@gmail.com>
2026-06-26 14:54:41 +0500

    Update duo_kb.py

--- keyboards/duo_kb.py
+++ keyboards/duo_kb.py
@@ -3,9 +3,9 @@ from aiogram.types import InlineKeyboardButton
 
 def get_duo_menu_keyboard():
     builder = InlineKeyboardBuilder()
-    builder.row(InlineKeyboardButton(text="🌅 Tonggi duolar", callback_data="duo_tong"))
-    builder.row(InlineKeyboardButton(text="🌃 Kechki duolar", callback_data="duo_kech"))
-    builder.row(InlineKeyboardButton(text="🚗 Safar duolari", callback_data="duo_safar"))
-    builder.row(InlineKeyboardButton(text="😴 Uyqu duolari", callback_data="duo_uyqu"))
+    builder.row(InlineKeyboardButton(text="🌅 Tonggi duolar", callback_data="duo_tong_0"))
+    builder.row(InlineKeyboardButton(text="🌃 Kechki duolar", callback_data="duo_kech_0"))
+    builder.row(InlineKeyboardButton(text="🚗 Safar duolari", callback_data="duo_safar_0"))
+    builder.row(InlineKeyboardButton(text="😴 Uyqu duolari", callback_data="duo_uyqu_0"))
     builder.row(InlineKeyboardButton(text="⬅️ Asosiy menyu", callback_data="back_to_menu"))
     return builder.as_markup()
