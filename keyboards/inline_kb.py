b7549d9ea03c267f417a2917f46e1f3ad00eda75
ruslanyootuber-ux <ruslanyootuber@gmail.com>
2026-06-26 10:41:38 +0500

    Update inline_kb.py

--- keyboards/inline_kb.py
+++ keyboards/inline_kb.py
@@ -24,7 +24,9 @@ def get_regions_keyboard() -> InlineKeyboardMarkup:
     # Hadis hikmatlar tugmasi
     builder.row(InlineKeyboardButton(text="📖 Kun hadisi", callback_data="menu_hadis"))
     #duo tugmasi
-    InlineKeyboardButton(text="🤲 Duolar", callback_data="menu_duo")
+        #duo tugmasi
+    builder.row(InlineKeyboardButton(text="🤲 Duolar", callback_data="menu_duo"))
+InlineKeyboardButton(text="🤲 Duolar", callback_data="menu_duo")
     # Qibla tugmasi
     builder.row(InlineKeyboardButton(text="🕋 Qibla yo'nalishi", callback_data="menu_qibla"))
 
