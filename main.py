73e25c47318bb06f267f67bbb5dcca69ccc094a2
ruslanyootuber-ux <ruslanyootuber@gmail.com>
2026-06-26 05:45:02 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from loader import bot, dp
 # admin_handlers ni import qilgan ekansiz, uni ham qo'shamiz
 from handlers import start, menu_handlers, extra_handlers, admin_handlers
 from handlers.extra_handlers import check_and_send_reminders
-from handlers.zikr_handlers import router as zikr_router 
+from handlers.zikr_handlers import router as zikr_router
+from handlers.ramadan_handlers import router as ramadan_router
 
 async def on_startup():
     print("Bot muvaffaqiyatli ishga tushdi!")
@@ -25,7 +26,8 @@ async def main():
         menu_handlers.router,
         extra_handlers.router,
         admin_handlers.router, # Bu yerda admin_handlers qo'shildi
-        zikr_router            # Bu yerda qavs keraksiz, shunchaki vergul bilan qo'yiladi
+        zikr_router,
+ramadan_router           # Bu yerda qavs keraksiz, shunchaki vergul bilan qo'yiladi
     )
 
     dp.startup.register(on_startup)
