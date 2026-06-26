2ef68126cd465541112f77986dd19b15ce13d05f
ruslanyootuber-ux <ruslanyootuber@gmail.com>
2026-06-26 07:24:31 +0500

    Create data/tasbeh_data.py

---
+++ data/tasbeh_data.py
@@ -0,0 +1,7 @@
+# data/tasbeh_data.py
+TASBEH_ZIKRLARI = [
+    {"name": "Subhanalloh", "arabic": "سُبْحَانَ اللهِ"},
+    {"name": "Alhamdulillah", "text": "الْحَمْدُ للهِ"},
+    {"name": "Allohu Akbar", "text": "اللهُ أَكْبَرُ"},
+    {"name": "Astag'firulloh", "text": "أَسْتَغْفِرُ اللهَ"}
+]
