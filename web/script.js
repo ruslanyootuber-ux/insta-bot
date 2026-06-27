c06e5d37b1e22f66d333fea47b496a1e0b31fc88
ruslanyootuber-ux <ruslanyootuber@gmail.com>
2026-06-28 02:50:02 +0500

    Delete script.js

--- web/script.js
+++
@@ -1,7 +0,0 @@
-// web/script.js
-async function loadPrayerTimes() {
-    const res = await fetch("http://YOUR-FLY-URL.fly.dev:8080/api/namoz");
-    const data = await res.json();
-    document.getElementById('prayer-time').innerText = data.vaqtlar.bomdod;
-}
-loadPrayerTimes();
