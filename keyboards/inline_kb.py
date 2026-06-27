a9b5b938d12c369defbb05d5b6f0ed803d533ccf
ruslanyootuber-ux <ruslanyootuber@gmail.com>
2026-06-27 16:12:09 +0500

    Update inline_kb.py

--- keyboards/inline_kb.py
+++ keyboards/inline_kb.py
@@ -14,7 +14,7 @@ def get_main_menu_kb() -> InlineKeyboardMarkup:
     
     # Eskidan bor tugmalar
     builder.button(text="🕌 Namoz vaqtlari", callback_data="menu_regions")
-    builder.button(text="✨ Asmaul Husna", callback_data="menu_asmaul")
+    builder.button(text="✨ Allohning 99 ismi", callback_data="menu_asmaul")
     builder.button(text="📿 Elektron tasbeh", callback_data="menu_tasbeh")
     builder.button(text="🤲 Kunlik duolar", callback_data="menu_duo")
     builder.button(text="📖 Kunlik hadis", callback_data="menu_hadis")
@@ -22,7 +22,7 @@ def get_main_menu_kb() -> InlineKeyboardMarkup:
     builder.button(text="🤲 Kunlik zikrlar", callback_data="menu_zikr_main")
     builder.button(text="🌙 Ramazon taqvimi", callback_data="menu_ramadan")
     builder.button(text="🔔 Eslatma belgilash", callback_data="menu_reminder")
-    builder.button(text=" ☪️ Mazhabni tanlash", callback_data="menu_settings")
+    builder.button(text="☪️ Mazhabni tanlash", callback_data="menu_settings")
     builder.button(text="👨‍💻 Bog'lanish", callback_data="menu_creator")
     builder.button(text="⭐ Botni baholash", callback_data="menu_rate")
 
@@ -34,7 +34,8 @@ def get_main_menu_kb() -> InlineKeyboardMarkup:
     
     # 2. Do'stlarga ulashish tugmasi (bu ham maxsus link)
     # Ulashiladigan matnni ham shu yerda o'zgartirishingiz mumkin
-    share_text = "Foydali Namoz Vaqtlari botini sizga ham tavsiya qilaman!"
+    share_text = "🕌«Foydali Namoz Vaqtlari» boti — cho‘ntagingizdagi ishonchli yordamchi! 📱
+Aniqlik, qulaylik va barcha kerakli ma’lumotlar bir joyda. ✨ Boshlash uchun hoziroq ulaning!🔰"
     share_url = f"https://t.me/share/url?url=https://t.me/{BOT_USERNAME}&text={share_text}"
     builder.row(InlineKeyboardButton(text="📲 Do'stlarga ulashish", url=share_url))
 
