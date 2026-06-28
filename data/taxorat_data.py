7c7c38e253c071576a5a1df17532fbf83315ae20
ruslanyootuber-ux <ruslanyootuber@gmail.com>
2026-06-28 17:09:12 +0500

    Create taxorat_data.py

---
+++ data/taxorat_data.py
@@ -0,0 +1,17 @@
+# Tahorat qadamlari lug'ati
+# 'gif_id' ga botga GIF yuborib olingan FILE_ID ni qo'yasiz.
+TAHORAT_STEPS = [
+    {
+        "step": 1,
+        "text_latin": "<b>1. Niyat</b>\n\nTahorat olish uchun, iloji bo'lsa, qiblaga yuzlaniladi.\n«Bismillahir rohmanir rohiym» deb, niyat qilinadi.",
+        "text_cyrillic": "<b>1. Ният</b>\n\nТаҳорат олиш учун, иложи бўлса, қиблага юзланилади.\n«Бисмиллаҳир роҳманир роҳийм» деб, ният қилинади.",
+        "gif_id": "8727877170" 
+    },
+    {
+        "step": 2,
+        "text_latin": "<b>2. Qo'llarni yuvish</b>\n\nQo'llar bandigacha uch marta yuviladi. Barmoqida uzugi bor kishi uni qimirlatib, ostiga suv yetkazadi. Barmoqlarni bir-birining orasiga kirgiziladi.",
+        "text_cyrillic": "<b>2. Қўлларни ювиш</b>\n\nҚўллар бандигача уч марта ювилади. Бармоғида узуги бор киши уни қимирлатиб, остига сув етказади. Бармоқларни бир-бирининг орасига киргизилади.",
+        "gif_id": "8727877170"
+    }
+    # Qolgan qadamlarni ham xuddi shu shaklda qo'shib chiqasiz...
+]
