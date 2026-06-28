6c3273bbd02d8fe8d537616e9a2373a8f7f28725
ruslanyootuber-ux <ruslanyootuber@gmail.com>
2026-06-28 17:50:44 +0500

    Update taxorat_data.py

--- data/taxorat_data.py
+++ data/taxorat_data.py
@@ -5,13 +5,13 @@ TAHORAT_STEPS = [
         "step": 1,
         "text_latin": "<b>1. Niyat</b>\n\nTahorat olish uchun, iloji bo'lsa, qiblaga yuzlaniladi.\n«Bismillahir rohmanir rohiym» deb, niyat qilinadi.",
         "text_cyrillic": "<b>1. Ният</b>\n\nТаҳорат олиш учун, иложи бўлса, қиблага юзланилади.\n«Бисмиллаҳир роҳманир роҳийм» деб, ният қилинади.",
-        "gif_id": "8727877170" 
+        "gif_id": "CgACAgIAAxkBAAN8akEYTIBoT1f10folzl2_D0oFcGMAAhKeAALo1AlK8OGKdcrnrDQ8BA" 
     },
     {
         "step": 2,
         "text_latin": "<b>2. Qo'llarni yuvish</b>\n\nQo'llar bandigacha uch marta yuviladi. Barmoqida uzugi bor kishi uni qimirlatib, ostiga suv yetkazadi. Barmoqlarni bir-birining orasiga kirgiziladi.",
         "text_cyrillic": "<b>2. Қўлларни ювиш</b>\n\nҚўллар бандигача уч марта ювилади. Бармоғида узуги бор киши уни қимирлатиб, остига сув етказади. Бармоқларни бир-бирининг орасига киргизилади.",
-        "gif_id": "8727877170"
+        "gif_id": "CgACAgIAAxkBAAN-akEYlsMRpToGxjnWCShEcPv89pkAAhaeAALo1AlKetnCqcKrnl08BA"
     }
     # Qolgan qadamlarni ham xuddi shu shaklda qo'shib chiqasiz...
 ]
