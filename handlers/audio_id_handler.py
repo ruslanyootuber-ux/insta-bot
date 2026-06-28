e36454e854ee8570a82a6023c4e6588075c7e97b
ruslanyootuber-ux <ruslanyootuber@gmail.com>
2026-06-29 00:43:46 +0500

    Create handlers/audio_id_handler.py

---
+++ handlers/audio_id_handler.py
@@ -0,0 +1,28 @@
+# handlers/audio_id_handler.py
+
+from aiogram import Router, F
+from aiogram.types import Message
+
+router = Router()
+
+# АДМИН ИД рақамингизни шу ерга ёзиб қўйинг (масалан: 12345678)
+ADMIN_ID = 12345678  
+
+@router.message(F.audio)
+async def get_audio_file_id(message: Message):
+    """Агар аудио юборилса, унинг Telegram File ID сини қайтаради"""
+    if message.from_user.id != ADMIN_ID:
+        return # Админ бўлмаса, ҳеч нарса қилмайди
+
+    audio_id = message.audio.file_id
+    audio_name = message.audio.file_name or "Номсиз аудио"
+    
+    response_text = (
+        f"🎵 <b>Аудио файл қабул қилинди!</b>\n\n"
+        f"<b>Номи:</b> {audio_name}\n"
+        f"<b>Код учун File ID:</b>\n"
+        f"<code>{audio_id}</code>\n\n"
+        f"<i>Ушбу ИД ни нусхалаб, suralar_data.py ичидаги тегишли суранинг 'audio_id' қисмига қўйинг.</i>"
+    )
+    
+    await message.reply(text=response_text, parse_mode="HTML")
