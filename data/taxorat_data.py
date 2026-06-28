27efb983d1fdfe98617400071fb0676a253b3b59
ruslanyootuber-ux <ruslanyootuber@gmail.com>
2026-06-28 18:29:59 +0500

    Update taxorat_data.py

--- data/taxorat_data.py
+++ data/taxorat_data.py
@@ -1,17 +1,12 @@
-# Tahorat qadamlari lug'ati
-# 'gif_id' ga botga GIF yuborib olingan FILE_ID ni qo'yasiz.
 TAHORAT_STEPS = [
     {
         "step": 1,
-        "text_latin": "<b>1. Niyat</b>\n\nTahorat olish uchun, iloji bo'lsa, qiblaga yuzlaniladi.\n«Bismillahir rohmanir rohiym» deb, niyat qilinadi.",
-        "text_cyrillic": "<b>1. Ният</b>\n\nТаҳорат олиш учун, иложи бўлса, қиблага юзланилади.\n«Бисмиллаҳир роҳманир роҳийм» деб, ният қилинади.",
-        "gif_id": "CgACAgIAAxkBAAN8akEYTIBoT1f10folzl2_D0oFcGMAAhKeAALo1AlK8OGKdcrnrDQ8BA" 
+        "text": "<b>1. Ният</b>\n\nТаҳорат олиш учун, иложи бўлса, қиблага юзланилади.\n«Бисмиллаҳир роҳманир роҳийм» деб, ният қилинади.",
+        "gif_id": "FILE_ID_1" 
     },
     {
         "step": 2,
-        "text_latin": "<b>2. Qo'llarni yuvish</b>\n\nQo'llar bandigacha uch marta yuviladi. Barmoqida uzugi bor kishi uni qimirlatib, ostiga suv yetkazadi. Barmoqlarni bir-birining orasiga kirgiziladi.",
-        "text_cyrillic": "<b>2. Қўлларни ювиш</b>\n\nҚўллар бандигача уч марта ювилади. Бармоғида узуги бор киши уни қимирлатиб, остига сув етказади. Бармоқларни бир-бирининг орасига киргизилади.",
-        "gif_id": "CgACAgIAAxkBAAN-akEYlsMRpToGxjnWCShEcPv89pkAAhaeAALo1AlKetnCqcKrnl08BA"
+        "text": "<b>2. Қўлларни ювиш</b>\n\nҚўллар бандигача уч марта ювилади. Бармоғида узуги бор киши ушни қимирлатиб, остига сув етказади. Бармоқларни бир-бирининг орасига киргизилади.",
+        "gif_id": "FILE_ID_2"
     }
-    # Qolgan qadamlarni ham xuddi shu shaklda qo'shib chiqasiz...
 ]
