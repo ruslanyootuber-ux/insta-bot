dc650873efc14e881c1d2d3efc752705b45a259e
ruslanyootuber-ux <ruslanyootuber@gmail.com>
2026-06-28 05:34:48 +0500

    Create savol_handlers.py

---
+++ handlers/savol_handlers.py
@@ -0,0 +1,7 @@
+@router.callback_query(F.data.startswith("faq_page_"))
+async def faq_pagination_handler(callback: types.CallbackQuery):
+    page = int(callback.data.split("_")[2])
+    await callback.message.edit_text(
+        "Quyidagi mavzulardan birini tanlang:",
+        reply_markup=get_savol_menu_kb(page)
+    )
