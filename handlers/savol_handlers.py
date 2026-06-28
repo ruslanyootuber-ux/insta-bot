bdef862368e0433feebd28f1efb3ac26bc0c6932
ruslanyootuber-ux <ruslanyootuber@gmail.com>
2026-06-28 13:08:14 +0500

    Update savol_handlers.py

--- handlers/savol_handlers.py
+++ handlers/savol_handlers.py
@@ -1,3 +1,5 @@
+from data.data_savol_javob import SAVOL_JAVOBLAR
+
 @router.callback_query(F.data.startswith("faq_page_"))
 async def faq_pagination_handler(callback: types.CallbackQuery):
     page = int(callback.data.split("_")[2])
