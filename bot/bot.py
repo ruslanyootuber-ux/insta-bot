094d7080d969624a365a1782236e89fff5e779fe
ruslanyootuber-ux <ruslanyootuber@gmail.com>
2026-06-29 22:52:07 +0500

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -1,7 +1,11 @@
 from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
 import os
 
-bot = Bot(token=os.getenv("BOT_TOKEN"))
+# Token .env yoki Fly.io Secrets'dan olinadi
+bot = Bot(
+    token=os.getenv("BOT_TOKEN"),
+    default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+)
 dp = Dispatcher()
-
-# Botingizning handlerlari shu yerda...
