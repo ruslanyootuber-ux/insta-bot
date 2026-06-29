e49c1674d80cbb77d901bb520215af639f6358c4
ruslanyootuber-ux <ruslanyootuber@gmail.com>
2026-06-30 02:14:48 +0500

    Update loader.py

--- loader.py
+++ loader.py
@@ -1,13 +1,9 @@
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
-from aiogram.fsm.storage.memory import MemoryStorage
 from config import BOT_TOKEN
-
-# MemoryStorage - FSM (holatlar) uchun vaqtinchalik xotira
-storage = MemoryStorage()
 
 # Bot instansiyasi
 bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode="HTML"))
 
-# Dispatcherga storage ni ulaymiz
-dp = Dispatcher(storage=storage)
+# Dispatcher (storage'siz sodda holatda)
+dp = Dispatcher()
