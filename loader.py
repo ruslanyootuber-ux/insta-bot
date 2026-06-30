801f7e340341de1105242f3e1f3f012b91b7dbb8
ruslanyootuber-ux <ruslanyootuber@gmail.com>
2026-06-30 13:13:12 +0500

    Update loader.py

--- loader.py
+++ loader.py
@@ -1,9 +1,15 @@
+# loader.py
+
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from config import BOT_TOKEN
+from database.db import Database  # Ma'lumotlar bazasi klassini import qilamiz
 
-# Bot instansiyasi
+# Bot instansiyasi (HTML rejimida)
 bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode="HTML"))
 
-# Dispatcher (storage'siz sodda holatda)
+# Dispatcher instansiyasi
 dp = Dispatcher()
+
+# Ma'lumotlar bazasini ishga tushiramiz va 'db' o'zgaruvchisini yaratamiz
+db = Database("data/main.db")  # Agar baza fayli boshqa yerda bo'lsa, manzilni o'zgartirishingiz mumkin
